38c21fe63adc60d78d9a1f568d925d4b9ee74fee
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-03-22 18:40:59 +0100

    Add files via upload
    Module 3, Level 13: JavaScript if, else

---
+++ script.js
@@ -0,0 +1,21 @@
+let age = 18;
+let isFemale = true;
+let driverStatus = 'bob';
+
+if (age > 17) {
+    console.log('You are old enough');
+} else {
+    console.log('Sorry, you are too young to enter');
+}
+
+if (isFemale = true) {
+   console.log('You can purchase a ticket');
+} else {
+    console.log('Sorry, but tonight is ladies night');
+}
+
+if (driverStatus == 'bob') {
+    console.log('You can drive');
+} else {
+    console.log('You had too much to drink. Let us call a taxi');
+}
