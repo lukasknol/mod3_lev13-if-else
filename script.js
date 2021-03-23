f3fbd6ee84454c625eea5a78dd0696734471bbac
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-03-23 11:24:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-let age = 18;
-let isFemale = true;
-let driverStatus = 'bob';
+const myAge = 18;
+const isFemale = true;
+const driverStatus = 'bob';
 
 /*
-Test data
-const age = 16; 
-const isFemale = false;
-const driverStatus = Johnson; 
+let age = 16; 
+let isFemale = false;
+let driverStatus = 'Johnson'; 
 */
 
+/* 
+Previous code
 if (age >= 18) {
     console.log('Come inside and have a drink!');
 } else {
     console.log('Sorry, you are too young to enter');
 }
+*/
 
-if (isFemale = true) {
+if (myAge >= 18) {
+    console.log('You get a 50% discount');
+} else {
+    console.log('You have to pay the full amount');
+}
+
+if (isFemale) {
    console.log('You can purchase a ticket');
 } else {
     console.log('Sorry, but tonight is ladies night');
 }
 
-if (driverStatus == 'bob') {
+if (driverStatus === 'bob') {
     console.log('You can drive');
 } else {
     console.log('You had too much to drink. Let us call a taxi');
